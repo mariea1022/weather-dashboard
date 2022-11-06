84ef7b5a614960a82a9ca6ee1af1fbbda2f085fa
Marie Lewis <mariea1022@gmail.com>
2022-11-05 19:12:42 -0700

    modified searchCityForecase() to display all 5 future temp, wind & humidity on the webpage

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 // HOOKS to the DOM
 var searchButtonEl = document.querySelector(".btn-primary")
 var cityInputEl = document.querySelector("#form-1")
-var cityNameCurrentDateEl = document.querySelector("#cityNameCurrentDate")
+var cityNameCurrentDateEl = document.querySelector("#cityName")
+var currentDateEl = document.querySelector("#currentDate")
+var weatherIconEl = document.querySelector("#weatherIcon")
 var currentTempEl = document.querySelector("#currentTemp")
 var currentWindEl = document.querySelector("#currentWind")
 var currentHumidityEl = document.querySelector("#currentHumidity")
-var futureTempEl = document.querySelector(".futureTemp")
+var futureTempEl = document.querySelectorAll(".futureTemp")
+var futureWindEl = document.querySelectorAll(".futureWind")
+var futureHumidityEl = document.querySelectorAll(".futureHumidity")
 
 // state variables
 // const variables
@@ -31,9 +35,9 @@ function searchCoords() {
         return response.json();
     })
     .then(function (data) {
-        console.log(data[0].lat);
+        // console.log(data[0].lat);
         var latitude = data[0].lat
-        console.log(data[0].lon);
+        // console.log(data[0].lon);
         var longitude = data[0].lon
         searchCity(latitude, longitude)
         searchCityForecast(latitude, longitude)
@@ -41,7 +45,7 @@ function searchCoords() {
 }
 
 function searchCity(latitude, longitude) {
-    var requestURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b8508be034c21d15df5123b4ba8affbc"
+    var requestURL = "https://api.openweathermap.org/data/2.5/weather?lat=" + latitude + "&lon=" + longitude + "&appid=b8508be034c21d15df5123b4ba8affbc&units=imperial"
 
     fetch(requestURL)
     .then(function (response) {
@@ -50,20 +54,24 @@ function searchCity(latitude, longitude) {
     .then(function (data) {
         // WHEN I view current weather conditions for that city
         // THEN I am presented with the city name, the date, an icon representation of weather conditions, the temperature, the humidity, and the wind speed
-        var currentTemp = (data.main.temp - 273.15) * 9/5 + 32 + " F" 
+        // console.log(data.weather[0].icon)
+        var weatherIcon = data.weather[0].icon
+        var iconURL = "http://openweathermap.org/img/w/" + weatherIcon + ".png";
+        weatherIconEl.setAttribute("src", iconURL)
+        var currentTemp = "Temp: " + data.main.temp 
         currentTempEl.textContent = currentTemp
-        var currentWind = data.wind.speed + " MPH"
+        var currentWind = "Wind: " + data.wind.speed + " MPH"
         currentWindEl.textContent = currentWind
-        var currentHumidity = data.main.humidity + " %"
+        var currentHumidity = "Humidity: " + data.main.humidity + "%"
         currentHumidityEl.textContent = currentHumidity
-
-        
-        
     })
 }
 
+  
+// WHEN I view future weather conditions for that city
+// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 function searchCityForecast(latitude, longitude) {
-    var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=b8508be034c21d15df5123b4ba8affbc"
+    var requestURL = "https://api.openweathermap.org/data/2.5/forecast?lat=" + latitude + "&lon=" + longitude + "&appid=b8508be034c21d15df5123b4ba8affbc&units=imperial"
 
     fetch(requestURL)
     .then(function (response) {
@@ -72,20 +80,18 @@ function searchCityForecast(latitude, longitude) {
     .then(function (data) {
     // console.log(data)
     for (var i = 0; i < 5; i++) {
-    console.log(data.list[i].main.temp)
-    var futureTemp = data.list[i].main.temp
-    futureTempEl.textContent = futureTemp
-    console.log(data.list[i].wind.speed)
-    console.log(data.list[i].main.humidity)
+    // console.log(data.list[i].main.temp)
+    var futureTemp = "Temp: " + data.list[i].main.temp 
+    futureTempEl[i].textContent = futureTemp
+    // console.log(data.list[i].wind.speed)
+    var futureWind = "Wind: " + data.list[i].wind.speed + " MPH"
+    futureWindEl[i].textContent = futureWind
+    // console.log(data.list[i].main.humidity)
+    var futureHumidity = "Humidity: " + data.list[i].main.humidity
+    futureHumidityEl[i].textContent = futureHumidity
     }
 })
 }
-
-
-
-  
-// WHEN I view future weather conditions for that city
-// THEN I am presented with a 5-day forecast that displays the date, an icon representation of weather conditions, the temperature, the wind speed, and the humidity
 
 // WHEN I click on a city in the search history
 // THEN I am again presented with current and future conditions for that city
